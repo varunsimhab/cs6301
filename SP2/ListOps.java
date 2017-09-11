a54494d33677741a38b4e056ae8d99c0a6db2fd5
Sarath <sarath2624@gmail.com>
2017-09-10 20:07:59 -0500

    SP2 - Prob 1 - Added test cases.

--- SP2/ListOps.java
+++ SP2/ListOps.java
@@ -1,17 +1,7 @@
 import java.util.*;
 
 /**
- * Created by Sarath on 9/10/2017.
- */
-/**
- * Created by
- * Group 50
- *
- * Varun Simha Balaraju
- * Venkata Sarath Chandra Prasad Nelapati
- * Jithin Paul
- * Sunit Mathew
- * on 9/10/2017.
+ * Created by Sarath on 9/6/2017.
  */
 public class ListOps<T extends Comparable<? super T>> {
 
@@ -22,6 +12,8 @@ public class ListOps<T extends Comparable<? super T>> {
 
         T item1 = iterator1.hasNext() ? iterator1.next() : null;
         T item2 = iterator2.hasNext() ? iterator2.next() : null;
+
+        // to eliminate duplicates
         T lastAdded = null;
 
         while (item1 != null && item2 != null) {
@@ -65,6 +57,7 @@ public class ListOps<T extends Comparable<? super T>> {
 
     }
 
+
     public static <T extends Comparable<? super T>> void
     intersect(List<T> list1, List<T> list2, List<T> outList) {
         Iterator<T> iterator1 = list1.iterator();
@@ -74,6 +67,8 @@ public class ListOps<T extends Comparable<? super T>> {
         T item2 = iterator2.hasNext() ? iterator2.next() : null;
 
         while (item1 != null && item2 != null) {
+            // add to outList only if both list has a common element or else increment
+            // one of the list accordingly
             if (item1.compareTo(item2) == 0) {
                 outList.add(item1);
                 item1 = iterator1.hasNext() ? iterator1.next() : null;
@@ -93,9 +88,15 @@ public class ListOps<T extends Comparable<? super T>> {
 
         T item1 = iterator1.hasNext() ? iterator1.next() : null;
         T item2 = iterator2.hasNext() ? iterator2.next() : null;
+
+        // to eliminate duplicates
         T lastAdded = null;
 
+        /*
+        * Iterates till first list goes empty.
+         */
         while (item1 != null) {
+            // if second list is empty just copy all the elements in first list
             if (item2 == null) {
                 if (lastAdded == null || lastAdded.compareTo(item1) != 0) {
                     outList.add(item1);
@@ -120,26 +121,34 @@ public class ListOps<T extends Comparable<? super T>> {
     public static void main(String[] args) throws InstantiationException, IllegalAccessException {
         ListOps<Integer> ops = new ListOps<>();
         Random rand = new Random();
-        List<Integer> li1 = new ArrayList<>();
-        List<Integer> li2 = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            li1.add(rand.nextInt(10));
-            li2.add(rand.nextInt(10));
+
+        List<Integer> li1 = null, li2 = null;
+        int testCases = 3;
+        System.out.println("=============================================================================");
+        while (testCases-- > 0) {
+            System.out.println("Test Case : "+(3-testCases)+"\n");
+            li1 = new ArrayList<>();
+            li2 = new ArrayList<>();
+            for (int i = 0; i < 20; i++) {
+                li1.add(rand.nextInt(20));
+                li2.add(rand.nextInt(20));
+            }
+            Collections.sort(li1);
+            Collections.sort(li2);
+            List<Integer> out = new ArrayList<>();
+            intersect(li1, li2, out);
+            System.out.println("List 1: " + li1);
+            System.out.println("\nList 2: " + li2);
+            System.out.println("\nIntersection: " + out);
+
+            out = new ArrayList<>();
+            union(li1, li2, out);
+            System.out.println("\nUnion: " + out);
+
+            out = new ArrayList<>();
+            difference(li1, li2, out);
+            System.out.println("\nDifference : " + out);
+            System.out.println("=============================================================================");
         }
-        Collections.sort(li1);
-        Collections.sort(li2);
-        List<Integer> out = new ArrayList<>();
-        intersect(li1, li2, out);
-        System.out.println("List 1: " + li1);
-        System.out.println("List 2: " + li2);
-        System.out.println("Intersection: " + out);
-
-        out = new ArrayList<>();
-        union(li1, li2, out);
-        System.out.println("Union: " + out);
-
-        out = new ArrayList<>();
-        difference(li1, li2, out);
-        System.out.println("Difference : " + out);
     }
 }
