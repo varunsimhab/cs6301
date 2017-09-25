a55c1ad19f26d5981d38873ba445d57b86294f0c
Jitin <paul.jitin@gmail.com>
2017-09-24 20:15:57 -0500

    L3

--- src/cs6301/g50/LP1L3.java
+++ src/cs6301/g50/LP1L3.java
@@ -1,18 +1,105 @@
-// Starter code for Level 3 driver for lp1
+/*
+ * Created by
+ * Group 50
+ *
+ * Varun Simha Balaraju
+ * Venkata Sarath Chandra Prasad Nelapati
+ * Jithin Paul
+ * Sunit Mathew
+ * on 9/19/2017.
+ */
 
-// Change following line to your group number
 package cs6301.g50;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
+import java.util.regex.Pattern;
 
 public class LP1L3 {
+	ArrayList<Num> numFinalList = new ArrayList<Num>();
+	Num tempList[] = new Num[26];
+    
 
-       public static void main(String[] args) {
-	Scanner in = new Scanner(System.in);
-	LP1L3 x = new LP1L3();
 	
-	while(in.hasNext()) {
-	    String word = in.next();
-	    // To do ...
-	}
+    public void evaluate(String line) {
+    	if(line.length()==1) {
+    		int index = Character.getNumericValue(line.charAt(0)) -  Character.getNumericValue('a');
+    		numFinalList.add(tempList[index]);
+    	}
+    	else if(line.contains("+") || line.contains("-") || line.contains("*") || line.contains("/")
+    				|| line.contains("%") || line.contains("^") || line.contains("|")) {
+    		int index = Character.getNumericValue(line.charAt(0)) -  Character.getNumericValue('a');
+    		Stack<Num> st = new Stack<Num>();    		
+    		for(int i=2;i<line.length();i++) {
+    			char c = line.charAt(i); 
+    			switch (c){
+    				    case '+':
+    					    st.push(Num.add(st.pop(),st.pop()));
+    					    break;
+    				    case '-':
+    					    st.push(Num.subtract(st.pop(),st.pop()));
+    					    break;
+    				    case '*':
+    					    st.push(Num.product(st.pop(),st.pop()));
+    					    break;
+    				    case '/':
+    					    st.push(Num.divide(st.pop(),st.pop()));
+    					    break;
+    				    case '%':
+    					    st.push(Num.mod(st.pop(),st.pop()));
+    					    break;
+    				    case '^':
+    				    	Num a = st.pop();
+    				    	Num b = st.pop();
+    					    st.push(Num.power(b,a));
+    					    break;
+    				    case '|':
+    					    st.push(Num.squareRoot(st.pop()));
+    					    break;
+    				    default:
+    				    	st.push(tempList[Character.getNumericValue(c)- Character.getNumericValue('a')]);
+    				    	break;
+    				}
+    		}
+    		tempList[index] = st.pop();
+    		numFinalList.add(tempList[index]);   		
+    	}
+    	else {
+    		int index = Character.getNumericValue(line.charAt(0)) -  Character.getNumericValue('a'); 
+    		tempList[index] = new Num(line.substring(2));
+    		numFinalList.add(tempList[index]);
+    	}
     }
+	 
+    
+    
+    public void printResult() {
+        for(Num tp:numFinalList)
+        	System.out.println(tp);
+    }
+    
+    
+    
+	public static void main(String[] args) throws FileNotFoundException{
+    	   Scanner in;
+    	   if (args.length > 0) {
+               File inputFile = new File(args[0]);
+               in = new Scanner(inputFile);
+           } else
+               in = new Scanner(System.in);
+
+	       LP1L3 x = new LP1L3();	
+	       while(in.hasNext()) {
+	            String line = in.nextLine();
+	            line = line.replaceAll("\\s","");
+	            if(line.equals(";"))
+	            	break;
+	            line = line.substring(0, line.length() - 1);
+                x.evaluate(line);
+	       }
+	       x.printResult();
+	       in.close();
+        }
 }
