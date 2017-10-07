c09c24886da33834e935134f057e2ee1e4aa7116
Sunit Mathew <mathewsunit@gmail.com>
2017-10-07 16:02:31 -0500

    Some chanegs to the package in Test Files.

--- src/cs6301/g50/LP1L2.java
+++ src/cs6301/g50/LP1L2.java
@@ -6,11 +6,8 @@ package cs6301.g50;
 public class LP1L2 {
     public static void main(String[] args) {
 	Num x = new Num(999);
-	Num y = new Num("8");
-	Num z = Num.add(x, y);
+	Num z = Num.squareRoot(x);
 	System.out.println(z);
-	Num a = Num.power(x, y);
-	System.out.println(a);
 	z.printList();
     }
 }
